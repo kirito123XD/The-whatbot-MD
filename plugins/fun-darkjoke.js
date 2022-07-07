72a0deeb39687dc351888c400a262810d10a2ab2
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-07-07 10:36:18 -0300

    Delete plugins directory

--- plugins/fun-darkjoke.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-let rest = 'https://api.zacros.my.id/randomimg/darkjokes'
-    conn.sendButtonImg(m.chat, rest, 'oscuro? 🤨', wm2, '𝚂𝚒𝚐𝚞𝚒𝚎𝚗𝚝𝚎', '.darkjokes', m)
-}
-handler.help = ['darkjokes']
-handler.tags = ['internet', 'fun']
-handler.command = /^(dragjokes|darkjokes)$/i
-
-module.exports = handler
